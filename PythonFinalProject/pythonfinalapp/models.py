d0ac9c88db1bee713b16ac8bc52555e3a7f8e54d
tailsofjuniper <tailsofjuniper@gmail.com>
2020-06-22 16:52:45 -0700

    commit

--- PythonFinalProject/pythonfinalapp/models.py
+++ PythonFinalProject/pythonfinalapp/models.py
@@ -19,42 +19,11 @@ class Todo(models.Model):
 
 class Details(models.Model):
     todoid=models.ForeignKey(Todo, on_delete=models.DO_NOTHING)
-    todoattendance=models.ManyToManyField(User)
-    minutestext=models.TextField()
+    todoname=models.ForeignKey(Todo, on_delete=models.DO_NOTHING)
+    todoagenda=models.TextField()
     
     def __str__(self):
         return self.todoid
  
     class Meta:
-        db_table='todominutes'
-
-class Resource(models.Model):
-    resourcename=models.CharField(max_length=255)
-    resourcype=models.CharField(max_length=255)
-    resourceurl=models.URLField
-    resourcedateentered=models.DateField()
-    userid=models.ForeignKey(User, on_delete=models.DO_NOTHING)
-
-    
-    def __str__(self):
-        return self.resourcename
- 
-    class Meta:
-        db_table='resource'
-        verbose_name_plural='resources'
-
-class Event(models.Model):
-    eventtitle=models.CharField(max_length=255)
-    eventlocation=models.CharField(max_length=255)
-    eventdate=models.DateField()
-    eventtime=models.TimeField()
-    eventdescription=models.CharField(max_length=255)
-    eventuserid=models.ForeignKey(User, on_delete=models.DO_NOTHING)
-
-    
-    def __str__(self):
-        return self.eventtitle
- 
-    class Meta:
-        db_table='event'
-        verbose_name_plural='events'
+        db_table='tododetails'
